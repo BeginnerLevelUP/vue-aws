47f83ff29b216b1a14dec4f32863f5af32fab9a1
Damion Morgan <only4school5680@gmail.com>
2024-05-21 16:42:22 -0400

    create server folder using sdkv3 wish me luck and create first bucket

---
+++ server/createBucket.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from 'uuid'
+import { CreateBucketCommand, S3Client } from '@aws-sdk/client-s3'
+const client = new S3Client({ region: 'us-east-2' })
+
+const createBucket= async () => {
+  const command = new CreateBucketCommand({
+    Bucket: 'user-media-' + uuidv4()
+  })
+
+  try {
+    const { Location } = await client.send(command)
+    console.log(`Bucket created with location ${Location}`)
+  } catch (err) {
+    console.error(err)
+  }
+}
+createBucket()
