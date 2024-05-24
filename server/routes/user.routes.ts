5d5a08f255a6dabbfa943b8d794bbc3ebe3097cc
Damion Morgan <only4school5680@gmail.com>
2024-05-23 22:07:36 -0400

    delete added crud operations almost completed created middleware to get user data

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -5,13 +5,41 @@ import {
   DynamoDBDocumentClient,
   ScanCommand,
   QueryCommand,
-  UpdateCommand
+  UpdateCommand,
+  DeleteCommand
 } from '@aws-sdk/lib-dynamodb'
 
 const client = new DynamoDBClient({ region: 'us-east-2' })
 const docClient = DynamoDBDocumentClient.from(client)
 const table = 'Users'
 export const userRouter = express.Router()
+
+interface CustomRequest extends Request {
+  currentUser?: { [key: string]: any }
+}
+// Middleware function to query user by username
+const getUserByUsername = async (req: CustomRequest, res: Response, next: NextFunction) => {
+  const usernameParam = req?.params?.username
+
+  const queryCommand = new QueryCommand({
+    TableName: table,
+    KeyConditionExpression: 'userName = :param',
+    ExpressionAttributeValues: {
+      ':param': usernameParam
+    },
+    ConsistentRead: true
+  })
+
+  try {
+    const response = await docClient.send(queryCommand)
+    const currentUser: { [key: string]: any } = response?.Items?.[0] ?? {}
+    if (req) req.currentUser = currentUser
+    next()
+  } catch (error) {
+    res.status(500).send(`Error querying user ${usernameParam}: ${error}`)
+    console.error(`Error querying user ${usernameParam}: ${error}`)
+  }
+}
 //Create
 
 //Read
@@ -55,57 +83,75 @@ userRouter.get('/users/:username', async (req: Request, res: Response, next: Nex
 })
 
 //Update
-userRouter.put('/users/:username', async (req: Request, res: Response, next: NextFunction) => {
-  const usernameParam = req?.params?.username
+userRouter.put(
+  '/users/:username',
+  getUserByUsername,
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const usernameParam = req?.currentUser?.userName
+    const currentUser: { [key: string]: any } = req?.currentUser ?? {}
+    try {
+      const updateExpression =
+        'set ' +
+        Object.keys(currentUser)
+          .filter((key) => key !== 'userName' && key !== 'createdAt') // Exclude userName and createdAt from update
+          .map((key) => `${key} = :${key}`)
+          .join(', ')
 
-  const queryCommand = new QueryCommand({
-    TableName: table,
-    KeyConditionExpression: 'userName = :param',
-    ExpressionAttributeValues: {
-      ':param': usernameParam
-    },
-    ConsistentRead: true
-  })
+      const expressionAttributeValues: { [key: string]: any } = {}
+      Object.keys(currentUser).forEach((key) => {
+        if (key !== 'userName' && key !== 'createdAt') {
+          // Exclude userName and createdAt from expressionAttributeValues
+          expressionAttributeValues[`:${key}`] = req.body[key] ?? currentUser[key]
+        }
+      })
 
-  try {
-    const response = await docClient.send(queryCommand)
-    const currentUser: { [key: string]: any } = response?.Items?.[0] ?? {}
+      const updateCommand = new UpdateCommand({
+        TableName: table,
+        Key: {
+          userName: usernameParam,
+          createdAt: currentUser?.createdAt
+        },
+        UpdateExpression: updateExpression,
+        ExpressionAttributeValues: expressionAttributeValues,
+        ReturnValues: 'ALL_NEW'
+      })
 
-    const updateExpression =
-      'set ' +
-      Object.keys(currentUser)
-        .filter((key) => key !== 'userName' && key !== 'createdAt') // Exclude userName and createdAt from update
-        .map((key) => `${key} = :${key}`)
-        .join(', ')
+      const updateResponse = await docClient.send(updateCommand)
+      res.status(200).send(`Update User ${usernameParam} : ${JSON.stringify(updateResponse)}`)
+      console.log(`Update User ${usernameParam} : ${JSON.stringify(updateResponse)}`)
+    } catch (error) {
+      res.status(500).send(`Error Updating User ${usernameParam} : ${error}`)
+      console.error(`Error Updating User ${usernameParam} : ${error}`)
+    }
 
-    const expressionAttributeValues: { [key: string]: any } = {}
-    Object.keys(currentUser).forEach((key) => {
-      if (key !== 'userName' && key !== 'createdAt') {
-        // Exclude userName and createdAt from expressionAttributeValues
-        expressionAttributeValues[`:${key}`] = req.body[key] ?? currentUser[key]
-      }
-    })
+    next()
+  }
+)
 
-    const updateCommand = new UpdateCommand({
+// Delete
+userRouter.delete(
+  '/users/:username',
+  getUserByUsername,
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const usernameParam = req?.currentUser?.userName
+    const currentUser: { [key: string]: any } = req?.currentUser ?? {}
+    const command = new DeleteCommand({
       TableName: table,
       Key: {
-        userName: usernameParam,
+        userName: currentUser?.userName,
         createdAt: currentUser?.createdAt
-      },
-      UpdateExpression: updateExpression,
-      ExpressionAttributeValues: expressionAttributeValues,
-      ReturnValues: 'ALL_NEW'
+      }
     })
-
-    const updateResponse = await docClient.send(updateCommand)
-    res.status(200).send(`Update User ${usernameParam} : ${JSON.stringify(updateResponse)}`)
-    console.log(`Update User ${usernameParam} : ${JSON.stringify(updateResponse)}`)
-  } catch (error) {
-    res.status(500).send(`Error Updating User ${usernameParam} : ${error}`)
-    console.error(`Error Updating User ${usernameParam} : ${error}`)
+    try {
+      const response = await docClient.send(command)
+      res.status(200).send(`User ${usernameParam} deleted: ${JSON.stringify(response)}`)
+      console.log(`User ${usernameParam} deleted: ${JSON.stringify(response)}`)
+      return response
+    } catch (error) {
+      res.status(500).send(`Error Deleting User ${usernameParam} : ${error}`)
+      console.error(`Error Deleting User ${usernameParam} : ${error}`)
+    }
+    next()
+    next()
   }
-
-  next()
-})
-
-//Delte
+)
