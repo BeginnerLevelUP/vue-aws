ba851f7819d73781f6582e5e2c8ee8319c67e1f7
Damion Morgan <only4school5680@gmail.com>
2024-05-23 21:43:17 -0400

    dynamically update a single user

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,13 +1,20 @@
 import * as express from 'express'
 import { type Request, type Response, type NextFunction } from 'express'
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient, ScanCommand, QueryCommand } from '@aws-sdk/lib-dynamodb'
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  QueryCommand,
+  UpdateCommand
+} from '@aws-sdk/lib-dynamodb'
 
 const client = new DynamoDBClient({ region: 'us-east-2' })
 const docClient = DynamoDBDocumentClient.from(client)
 const table = 'Users'
 export const userRouter = express.Router()
+//Create
 
+//Read
 userRouter.get('/users', async (req: Request, res: Response, next: NextFunction) => {
   const command = new ScanCommand({
     TableName: table
@@ -31,13 +38,13 @@ userRouter.get('/users/:username', async (req: Request, res: Response, next: Nex
     TableName: table,
     KeyConditionExpression: 'userName=:param',
     ExpressionAttributeValues: {
-      ":param": usernameParam
+      ':param': usernameParam
     },
     ConsistentRead: true
   })
   try {
     const response = await docClient.send(command)
-    res.status(200).send(`User ${usernameParam}: ${JSON.stringify(response)}`)
+    res.status(200).send(`User ${usernameParam}: ${JSON.stringify(response?.Items)}`)
     console.log(`User ${usernameParam}: ${JSON.stringify(response?.Items)}`)
     return response
   } catch (error) {
@@ -46,3 +53,59 @@ userRouter.get('/users/:username', async (req: Request, res: Response, next: Nex
   }
   next()
 })
+
+//Update
+userRouter.put('/users/:username', async (req: Request, res: Response, next: NextFunction) => {
+  const usernameParam = req?.params?.username
+
+  const queryCommand = new QueryCommand({
+    TableName: table,
+    KeyConditionExpression: 'userName = :param',
+    ExpressionAttributeValues: {
+      ':param': usernameParam
+    },
+    ConsistentRead: true
+  })
+
+  try {
+    const response = await docClient.send(queryCommand)
+    const currentUser: { [key: string]: any } = response?.Items?.[0] ?? {}
+
+    const updateExpression =
+      'set ' +
+      Object.keys(currentUser)
+        .filter((key) => key !== 'userName' && key !== 'createdAt') // Exclude userName and createdAt from update
+        .map((key) => `${key} = :${key}`)
+        .join(', ')
+
+    const expressionAttributeValues: { [key: string]: any } = {}
+    Object.keys(currentUser).forEach((key) => {
+      if (key !== 'userName' && key !== 'createdAt') {
+        // Exclude userName and createdAt from expressionAttributeValues
+        expressionAttributeValues[`:${key}`] = req.body[key] ?? currentUser[key]
+      }
+    })
+
+    const updateCommand = new UpdateCommand({
+      TableName: table,
+      Key: {
+        userName: usernameParam,
+        createdAt: currentUser?.createdAt
+      },
+      UpdateExpression: updateExpression,
+      ExpressionAttributeValues: expressionAttributeValues,
+      ReturnValues: 'ALL_NEW'
+    })
+
+    const updateResponse = await docClient.send(updateCommand)
+    res.status(200).send(`Update User ${usernameParam} : ${JSON.stringify(updateResponse)}`)
+    console.log(`Update User ${usernameParam} : ${JSON.stringify(updateResponse)}`)
+  } catch (error) {
+    res.status(500).send(`Error Updating User ${usernameParam} : ${error}`)
+    console.error(`Error Updating User ${usernameParam} : ${error}`)
+  }
+
+  next()
+})
+
+//Delte
