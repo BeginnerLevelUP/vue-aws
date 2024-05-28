68eead93a4ac099534b03c915a6e6172aa9f0b8d
Damion Morgan <only4school5680@gmail.com>
2024-05-28 19:29:36 -0400

    finally on the front end

---
+++ src/components/loginForm.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+<template>
+<h2 class="text-center">Login Form</h2>
+<form>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+
+    <button type="submit">Login</button>
+</form>
+</template>
---
+++ src/components/signupForm.vue
@@ -0,0 +1,17 @@
+<script>
+</script>
+<template>
+<h2  class="text-center">Sign Up Form</h2>
+<form>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br><br>
+
+    <label for="username">Email:</label>
+    <input type="text" id="email" name="email" required><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+
+    <button type="submit">Login</button>
+</form>
+</template>
